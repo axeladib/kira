a11d33dd2c4f738fe797534416562e245166807f
axeladib <nabil298adib@gmail.com>
2023-06-01 23:27:10 +0800

    descriptionjs : initial commit hero text

--- src/app/description.js
+++ src/app/description.js
@@ -3,27 +3,39 @@ import "../../public/Kira/lighting.png";
 
 const Description = () => {
   return (
-    <div className="flex jusitfy-center align-items-center flex-col">
-      <h4 className="uppercase">calculate easy</h4>
-      <h1>
-        Hassle-free calculate{" "}
-        <span>
-          <Image src="/Kira/lighting.png" width={50} height={50} />
-        </span>{" "}
-        <span className="uppercase">socso</span>,{" "}
-        <span className="uppercase">epf</span> and{" "}
-        <span className="uppercase">eis</span> with Kira
-        <span>
-          <Image src="/Kira/kira__white.png" width={50} height={50} />
-        </span>
-      </h1>
-      <p>Flow to calculate EPF, SOCSO, EIS and total</p>
-      <p>employees salary</p>
-      <ul>
-        <li><Image src="/Kira/kwsp logo.png" width={50} height={50}/></li>
-        <li><Image src="/Kira/logo perkeso.png" width={50} height={50}/></li>
-      </ul>
-      <button>Use Now</button>
+    <div className="w-[80rem] h-[30rem] flex jusitfy-center align-items-center flex-col relative ">
+      <h4 className="uppercase text-[1.4rem] absolute left-[33rem] font-[600] text-[#0ADFB2]">
+        calculate easy
+      </h4>
+      {/* HERO TEXT */}
+      <div className="absolute top-[1.5rem] left-[13rem] font-[600]">
+        <div className="flex">
+          <h1 className="text-[3.5rem]">Hassle-free calculate</h1>
+          <img
+            className="w-[8rem] h-[9rem] absolute left-[31rem] top-[-1rem]"
+            src="/Kira/lighting.png"
+          />
+          <h1 className="text-[3.5rem] ml-[4rem]">SOCSO, KWSP</h1>
+        </div>
+        <div className="flex absolute left-[13rem]">
+          <h1 className="text-[3.5rem]">and EIS with Kira</h1>
+          <img className="w-[6rem] h-[5rem]" src="/Kira/kira__white.png" />
+        </div>{" "}
+        {/* DESCRIPTION HERO TEXT */}
+        <div className="absolute font-[400] text-[1.5rem] top-[12rem] left-[14rem]">
+          <p>Flow to calculate EPF, SOCSO, EIS and total</p>
+          <p className="absolute left-[7rem]">employees salary</p>
+        </div>
+        <ul className="absolute flex top-[17rem] left-[20rem]">
+          <li>
+            <Image src="/Kira/kwsp logo.png" width={120} height={50} />
+          </li>
+          <li>
+            <Image src="/Kira/logo perkeso.png" width={80} height={50} />
+          </li>
+        </ul>
+        <button className="absolute top-[25rem] left-[25rem] text-[1.5rem]">Use Now</button>
+      </div>
     </div>
   );
 };
